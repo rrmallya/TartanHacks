8bb502596f329a4352a76f9eb29c07141c25e467
Ramya Mallya <ramya@ramyamallya.com>
2014-02-01 06:47:55 -0500

    location is broken

--- public/client.js
+++ public/client.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+/*window.onload = function() {
  
     var messages = [];
     var socket = io.connect('http://localhost:3700');
@@ -23,5 +23,61 @@ window.onload = function() {
         var text = field.value;
         socket.emit('send', { message: text });
     };
+ 
+}*/
+window.onload = function() {
+ 
+    var messages = [];
+    var socket = io.connect('http://localhost:3700');
+    var field = document.getElementById("field");
+    var sendButton = document.getElementById("send");
+    var content = document.getElementById("content");
+    var usr;
+    socket.on('login', function (data) {
+        if(data.status=='Ready')
+        {
+            alert("ready to login!");
+        }
+    });
+ 
+    sendButton.onclick = function() {
+        usr = field.value;
+        socket.emit('username', { name: usr });
+        console.log(usr);
+    };
+    socket.on('pageLoad',function (data){
+        alert("message received");
+            if(data.page=='helpFeed'){
+            var helpReq=prompt("What do you need "+usr+"?");
+            var loc;
+            if (navigator.geolocation)
+    {
+    navigator.geolocation.getCurrentPosition(function(position){
+      var latlng=position.coords.latitude+","+position.coords.longitude;
+      console.log(latlng);
+      var xhr = new XMLHttpRequest();
+      xhr.open("GET","http://maps.googleapis.com/maps/api/geocode/json?latlng="+latlng+"&sensor=true" , false);
+      xhr.send();
+      console.log(xhr.status);
+      console.log(xhr.statusText);
+      xmlDocument = xhr.responseText;
+      var addresses=JSON.parse(xmlDocument);
+      console.log( addresses.results[0].formatted_address);
+      loc = addresses.results[0].formatted_address;
+        socket.emit('helpReq',{req:helpReq, locations:'loc',points:10});
+    });
+    }
+
+
+           
+        }
+        else
+            {console.log("Didn't load shit.");}
+
+    })
+    function findLocation(){
+
+        
+    }
  
 }
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 var port = 3700;
 var users=[];
-
+var posts=[];
 
 app.set('views', __dirname + '/views');
 app.set('view engine', "jade");
@@ -19,14 +19,24 @@ app.get("/", function(req, res){
 
 var io = require('socket.io').listen(app.listen(port));
 io.sockets.on('connection', function (socket) {
+
+io.set('log level', 1);
     
-    socket.emit('login', {login:'now'});
+    socket.emit('login', {status:'Ready'});
     var usrid=socket.id;
     socket.on('username', function (data) {
     	users=users.concat([{id:socket.id , name: data.name}]);
     	console.log(users);
         socket.emit('pageLoad', {page:'helpFeed'});
     });
+
+socket.on('helpReq',function (data){
+
+	posts.push();
+	io.sockets.emit('populate_posts',posts);
+	console.log(data);
+});
+
 });
 
 
