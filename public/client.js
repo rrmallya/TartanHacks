935677e270d68dd46acf2683abd04db66ca807ad
Chin Wei <chinwei.w@gmail.com>
2014-02-01 02:12:08 -0500

    made some client side changes

--- public/client.js
+++ public/client.js
@@ -1,27 +1,119 @@
-window.onload = function() {
- 
-    var messages = [];
-    var socket = io.connect('http://localhost:3700');
-    var field = document.getElementById("field");
-    var sendButton = document.getElementById("send");
-    var content = document.getElementById("content");
- 
-    socket.on('message', function (data) {
-        if(data.message) {
-            messages.push(data.message);
-            var html = '';
-            for(var i=0; i<messages.length; i++) {
-                html += messages[i] + '<br />';
-            }
-            content.innerHTML = html;
-        } else {
-            console.log("There is a problem:", data);
-        }
+var socket = io.connect('http://localhost');
+var username = {};
+
+
+
+
+
+
+
+
+
+    socket.on('login', function (data) {
+        // alert ('login');
+        $('#login-btn').show();
+        console.log(username);
+        // display login 
+
+        
+        // socket.emit('my other event', { my: 'data' });
     });
- 
-    sendButton.onclick = function() {
-        var text = field.value;
-        socket.emit('send', { message: text });
-    };
- 
+
+
+
+
+   
+
+
+window.fbAsyncInit = function() {
+    FB.init({
+        appId   : '346049582201169',
+        oauth   : true,
+        status  : true, // check login status
+        cookie  : true, // enable cookies to allow the server to access the session
+        xfbml   : true // parse XFBML
+    });
+
+};
+
+
+
+
+
+
+
+
+function fb_login(){
+    FB.login(function(response) {
+
+        if (response.authResponse) {
+            console.log('Welcome!  Fetching your information.... ');
+            //console.log(response); // dump complete info
+            access_token = response.authResponse.accessToken; //get access token
+            user_id = response.authResponse.userID; //get FB UID
+
+            FB.api('/me', function(response) {
+              
+                user_email = response.email; //get user email
+          // you can store this data into your database             
+            });
+
+        } else {
+            //user hit cancel button
+            console.log('User cancelled login or did not fully authorize.');
+
+        }
+    }, {
+        scope: 'publish_stream,email'
+    });
+
+
+     FB.Event.subscribe('auth.authResponseChange', function(response) {
+    // Here we specify what we do with the response anytime this event occurs. 
+    if (response.status === 'connected') {
+      // The response object is returned with a status field that lets the app know the current
+      // login status of the person. In this case, we're handling the situation where they 
+      // have logged in to the app.
+
+    console.log('logged in!');
+    console.log(response);
+    username = response.name;
+    socket.emit('username', { 
+        name: username
+    });
+
+
+
+      // testAPI();
+    } else if (response.status === 'not_authorized') {
+      // In this case, the person is logged into Facebook, but not into the app, so we call
+      // FB.login() to prompt them to do so. 
+      // In real-life usage, you wouldn't want to immediately prompt someone to login 
+      // like this, for two reasons:
+      // (1) JavaScript created popup windows are blocked by most browsers unless they 
+      // result from direct interaction from people using the app (such as a mouse click)
+      // (2) it is a bad experience to be continually prompted to login upon page load.
+      FB.login();
+    } else {
+      // In this case, the person is not logged into Facebook, so we call the login() 
+      // function to prompt them to do so. Note that at this stage there is no indication
+      // of whether they are logged into the app. If they aren't then they'll see the Login
+      // dialog right after they log in to Facebook. 
+      // The same caveats as above apply to the FB.login() call here.
+      FB.login();
+    }
+  }); 
+
 }
+(function() {
+    var e = document.createElement('script');
+    e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
+    e.async = true;
+    document.getElementById('fb-root').appendChild(e);
+}());
+
+
+
+
+
+   
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express = require("express");
 var app = express();
 var port = 3700;
 
+
+
+
  app.set('views', __dirname + '/views');
 app.set('view engine', "jade");
 app.engine('jade', require('jade').__express);
@@ -12,12 +15,26 @@ app.get("/", function(req, res){
 
  app.use(express.static(__dirname + '/public'));
 var io = require('socket.io').listen(app.listen(port));
+
+io.set('log level', 1);
+
+
 io.sockets.on('connection', function (socket) {
-    socket.emit('message', { message: 'welcome to the chat' });
-    socket.on('send', function (data) {
-        io.sockets.emit('message', data);
+    socket.emit('login', { message: 'welcome to the chat' });
+    
+
+
+
+    socket.on('username', function (data) {
+	console.log('username!!!');
+        // io.sockets.emit('message', data);
     });
+
+
+
 });
+
+
 
 
 console.log("Listening on port " + port);
